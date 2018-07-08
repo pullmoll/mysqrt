63cc36441b4ceebe77fff39663b4c700c5f36015
Jürgen Buchmüller <pullmoll@t-online.de>
2018-07-08 13:34:53 +0200

    Add COPYING (BSD-3-Clause).

--- mysqrt.c
+++ mysqrt.c
@@ -1,3 +1,34 @@
+/************************************************************************
+ * Copyright 2018 Jürgen Buchmüller <pullmoll@t-online.de>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *************************************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -19,7 +50,7 @@ static uint64_t digits;
 #define	SHIFT_MASK	((1<<SHIFT_BITS)-1)
 
 /**
- * @brief Rought estimate of the number of digits per mp_limb_t in base
+ * @brief Rough estimate of the number of digits per mp_limb_t in base
  * @param libs number of limbs
  * @param base number base
  */
@@ -36,7 +67,7 @@ digits_per_limb(uint64_t limbs, int base)
  * @param b fractional part of the number to print
  * @param bits number of bits in b
  * @param base base for output (e.g. 10)
- ****************************************************************/
+ */
 static char *
 float2str(mpz_t a, mpz_t b, uint64_t bits, int base)
 {
